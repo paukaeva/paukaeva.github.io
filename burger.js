8687526c5d3f7a0029119aa55cfef672b5541622
Анеечка <aneecka@MacBook-Air-Aneecka.local>
2022-06-25 14:29:33 +0800

    fix mobile anchors

--- burger.js
+++ burger.js
@@ -2,16 +2,16 @@ const iconMenu = document.querySelector('.menu_icon');
 const menuBody = document.querySelector('.menu_body');
 const menuItems = document.querySelectorAll('.header_main_nav a');
 
-iconMenu.addEventListener('click', function (e) {
-  document.body.classList.toggle('lock');
+iconMenu.addEventListener('click', (e) => {
+  // document.body.classList.toggle('lock');
   iconMenu.classList.toggle('active');
   menuBody.classList.toggle('active');
 });
 
-// menuItems.forEach((item) => {
-//   item.addEventListener('click', () => {
-//     document.body.classList.remove('lock');
-//     iconMenu.classList.remove('active');
-//     menuBody.classList.remove('active');
-//   })
-// })
+menuItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    // document.body.classList.remove('lock');
+    iconMenu.classList.remove('active');
+    menuBody.classList.remove('active');
+  })
+})
