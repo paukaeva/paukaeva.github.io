9f020af5f5f119ab598c247004e2d4ed8e393f04
Анеечка <aneecka@MacBook-Air-Aneecka.local>
2022-06-25 14:36:26 +0800

    remove unnecessary code

--- burger.js
+++ burger.js
@@ -2,15 +2,13 @@ const iconMenu = document.querySelector('.menu_icon');
 const menuBody = document.querySelector('.menu_body');
 const menuItems = document.querySelectorAll('.header_main_nav a');
 
-iconMenu.addEventListener('click', (e) => {
-  // document.body.classList.toggle('lock');
+iconMenu.addEventListener('click', () => {
   iconMenu.classList.toggle('active');
   menuBody.classList.toggle('active');
 });
 
 menuItems.forEach((item) => {
   item.addEventListener('click', () => {
-    // document.body.classList.remove('lock');
     iconMenu.classList.remove('active');
     menuBody.classList.remove('active');
   })
